6e194c1b5c289f2d5e432bbd161f896e3b60fe59
josemaestre22 <joseandresmaestre@gmail.com>
2023-02-13 16:06:56 -0400

    structure changes

--- main.lua
+++ main.lua
@@ -1,50 +1,14 @@
+-- Include Simple Tiled Implementation into project
+local sti = require "libraries/Simple-Tiled-Implementation/sti"
+
 function love.load()
-    -- Set filter to nearest to prevent bluriness when scaling sprites
-    love.graphics.setDefaultFilter("nearest", "nearest")
-    
-    -- Tilemap
-    require("map")
-    map:load()
-
-    --Player implementation
-    require("player")
-    player:load()
-
-    -- Enemies implementation
-    require("enemies")
-    enemies:load(4)
-
-    require("bullet")
+    map = sti("tiled_map/Sidescroller city map extended.lua")
 end
 
 function love.update(dt)
-    player:update(dt)
-
-    for i, enemy in ipairs(enemies) do
-        enemy:update(dt)
-    end 
-
-    for i, bullet in ipairs(bullets) do
-        bullet:update(dt, i)
-    end
+    map:update(dt)
 end
 
 function love.draw()
-    love.graphics.push() -- Used to be able to correctly dipsplay lives HUD without it being translated by the camera
-    love.graphics.translate(player.cam_x, 0) --Move the camera
     map:draw()
-    player:draw()
-    for i, enemy in ipairs(enemies) do
-        enemy:draw()
-    end
-
-    for i, bullet in ipairs(bullets) do
-        bullet:draw()
-    end
-
-    love.graphics.pop() -- Used to be able to correctly dipsplay lives HUD without it being translated by the camera
-
-    for i=0, player.lives - 1 do
-        love.graphics.draw(player.lives_image, (i * player.lives_image:getWidth() * 2.25 ), 0, 0, 4 , 4)
-    end
 end
---
+++ self_implemented_demo/bullet.lua
@@ -0,0 +1,57 @@
+bullets = {}
+
+-- Bullet class
+bullet = {
+    image = love.graphics.newImage("assets/Ball2.png"),
+    speed = 250
+}
+
+-- Bullet Constructor
+function bullet:new(entity)
+    object = {}   -- create object
+    setmetatable(object, self)
+    self.__index = self
+    object.width = self.image:getWidth()
+    object.height = self.image:getHeight()
+    object.x = entity.x
+    object.y = entity.y + entity.height 
+    object.direction = entity.direction
+    return object
+end
+
+function bullet.update(self, dt, bullet_number)
+    if self.x > 0 and self.x < map.width * map.tile_width then
+        if self.direction == "left" then
+            self.x = self.x - self.speed * dt
+        else
+            self.x = self.x + self.speed * dt
+        end
+        self:check_collision(player, bullet_number)
+    else 
+        table.remove(bullets, bullet_number)
+    end
+end
+
+function bullet.draw(self)
+    love.graphics.draw(self.image, self.x, self.y, 0, 2, 2)
+end
+
+function bullet.check_collision(self, obj, bullet_number)
+    local self_left = self.x
+    local self_right = self.x + self.width
+    local self_top = self.y
+    local self_bottom = self.y + self.height
+
+    local obj_left = obj.x
+    local obj_right = obj.x + obj.width
+    local obj_top = obj.y + 20
+    local obj_bottom = obj.y + (obj.height * obj.scale_y)
+    if  self_right > obj_left
+    and self_left < obj_right
+    and self_bottom > obj_top
+    and self_top < obj_bottom 
+    then
+        player.lives = player.lives - 1
+        table.remove(bullets, bullet_number)
+    end
+end
