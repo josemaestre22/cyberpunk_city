d17bd04e251fd5b6a6e15747563253ca76b2a5f1
josemaestre22 <joseandresmaestre@gmail.com>
2023-02-15 13:00:27 -0400

    minor tweaks

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
--- Include Simple Tiled Implementation into project
+-- Include Simple Tiled Implementation for tiled map
 local sti = require "libraries/Simple-Tiled-Implementation/sti"
+
+-- Include Bump library for collisions
 local bump = require"libraries/bump"
+
+-- Include Anim8 library for animations
+local anim8 = require"libraries/anim8"
 
 function love.load()
     map = sti("tiled_map/Sidescroller city map extended.lua")
